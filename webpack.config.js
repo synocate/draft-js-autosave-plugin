ffcf953936c8c73704402b7fed89eea1226ad642
Conner Jevning <cjevning@gmail.com>
2017-03-26 17:03:51 -0700

    fix things

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-var */
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var autoprefixer = require('autoprefixer');
-var webpack = require('webpack');
 
 module.exports = {
   output: {
@@ -14,22 +12,12 @@ module.exports = {
     loaders: [
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader?modules&importLoaders=1&localIdentName=draftJsAutosavePlugin__[local]__[hash:base64:5]!postcss-loader'
-        }),
+        loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader?modules&importLoaders=1&localIdentName=draftJsAutosavePlugin__[local]__[hash:base64:5]!postcss-loader' }),
       },
     ],
   },
+
   plugins: [
-    new ExtractTextPlugin(`${path.parse(process.argv[2]).name}.css`),
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        context: __dirname,
-        postcss: [
-          autoprefixer({ browsers: ['> 1%'] })
-        ]
-      }
-    })
+    new ExtractTextPlugin({ filename: `${path.parse(process.argv[2]).name}.css` }),
   ],
 };
